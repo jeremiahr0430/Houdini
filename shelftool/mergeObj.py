7b4df1a309f64c9cc277f4000c26a1a830053d5e
jeremiahr0430 <jeremiahr0430@yahoo.com>
2020-05-21 23:16:01 -0400

    Update mergeObj.py

--- shelftool/mergeObj.py
+++ shelftool/mergeObj.py
@@ -3,32 +3,53 @@ reload(generateNode)
 import hou
 
 def run():
-            
+    def finish_it(nullName,null_node,objMergeName):
+        null_node.setName(nullName)
+        nullName = null_node.name() # rehash name
+
+
+        objMergePath = null_node.path()
+        print "path to fill in on obj merge node created later is %s" % objMergePath
+
+
+        # create render object and object merge
+
+        renderObj = hou.node("/obj").createNode("geo","{}_render".format(objMergeName))
+        renderObjPath = renderObj.path()
+
+        objMerge = hou.node(renderObjPath).createNode("object_merge",objMergeName)
+
+        # reset parm for created object merge node
+        objMerge.parm("objpath1").set(objMergePath)
+
+        objMerge.parm("xformtype").set(1)
+
+        #select created render object
+        renderObj.setCurrent(True) 
+# Run Func Starts here !!!            
     null_node = generateNode.GenerateNode('null')
     null_node = null_node.generateNode()
 
     nullName = null_node.name()
-    null_node.setName('OUT_to_{}'.format(nullName))
-    nullName = null_node.name() # rehash name
+    objMergeName = nullName
+    nullName = 'OUT_to_{}'.format(nullName)
+    
+    nullParent = null_node.parent()
 
-
-    objMergePath = null_node.path()
-    print "path to fill in on obj merge node created later is %s" % objMergePath
-
-
-    # create render object and object merge
-
-    renderObj = hou.node("/obj").createNode("geo","{}_render".format(nullName))
-    renderObjPath = renderObj.path()
-
-    objMerge = hou.node(renderObjPath).createNode("object_merge",nullName)
-
-    # reset parm for created object merge node
-    objMerge.parm("objpath1").set(objMergePath)
-
-    objMerge.parm("xformtype").set(1)
-
-    #select created render object
-    renderObj.setCurrent(True) 
-
-
+# check if name exists   
+    existList = [kid for kid in nullParent.children() if kid.name() == nullName] 
+    if len(existList) >=1: 
+        # Ask if still continue
+        confirm = hou.ui.displayConfirmation('The is one object with the same name. Continue?', hou.severityType.Message, None, None,None,None, hou.confirmType.OverwriteFile)
+        if confirm == True:
+            increment = 0
+            # Apend a digit to the null name
+            while len(existList) >=1: 
+                increment +=1
+                nullName = "{}{}".format(nullName,increment)
+                existList = [kid for kid in nullParent.children() if kid.name() == nullName] 
+            finish_it(nullName,null_node,objMergeName)
+        else:
+            null_node.destroy()
+    else:
+        finish_it(nullName,null_node,objMergeName)
